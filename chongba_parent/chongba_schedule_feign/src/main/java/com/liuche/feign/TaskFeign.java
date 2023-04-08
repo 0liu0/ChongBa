5865d178796d87dc8ac5b2b86bf0d82ad5f98f30
liuche <706716852@qq.com>
2023-04-08 22:12:59 +0800

    4-4 整合了openfeign并且对现有的三个接口进行了测试，负载均衡

---
+++ chongba_parent/chongba_schedule_feign/src/main/java/com/liuche/feign/TaskFeign.java
@@ -0,0 +1,17 @@
+package com.liuche.feign;
+
+import com.liuche.common.entity.ResponseMessage;
+import com.liuche.common.entity.Task;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(value = "schedule-service",path = "/task")
+public interface TaskFeign {
+    @PostMapping("/push")
+    ResponseMessage pushTask(@RequestBody Task task);
+    @GetMapping("/poll/{taskType}/{priority}")
+    ResponseMessage pollTask(@PathVariable Integer priority, @PathVariable Integer taskType);
+    @GetMapping ("/cancel")
+    ResponseMessage cancelTask(@RequestParam("taskId") Long taskId);
+}
---
+++ chongba_parent/chongba_schedule_feign/src/test/java/com/liuche/feign/TaskFeignTest.java
@@ -0,0 +1,44 @@
+package com.liuche.feign;
+
+import com.liuche.TaskFeignApplication;
+import com.liuche.common.entity.ResponseMessage;
+import com.liuche.common.entity.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TaskFeignApplication.class)
+public class TaskFeignTest {
+    @Autowired
+    private TaskFeign taskFeign;
+    @Test
+    public void test01() {
+        for (int i = 0; i < 4; i++) {
+            Task task = new Task();
+            task.setTaskType(2003);
+            task.setParameters("testFeignClient".getBytes());
+            task.setPriority(100);
+            task.setExecuteTime(new Date().getTime());
+            ResponseMessage response = taskFeign.pushTask(task);
+            System.out.println(response);
+        }
+    }
+
+    @Test
+    public void test2(){
+        ResponseMessage message = taskFeign.pollTask(100, 2003);
+        System.out.println(message);
+    }
+
+    @Test
+    public void test3(){
+        taskFeign.cancelTask(1644704445173780482L);// 从数据库或缓存中查找一个任务id
+    }
+
+
+}
